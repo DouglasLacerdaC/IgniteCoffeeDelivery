41d0be5c6ba547b878e5194a78692995b9efb9b7
douglas lacerda <douglaslacerda544@gmail.com>
2023-07-08 08:59:15 -0300

    feat: Project creation and route configuration

---
+++ src/pages/Cart/index.tsx
@@ -0,0 +1,5 @@
+export function CartPage() {
+    return (
+        <h1>Hello Cart</h1>
+    )
+}
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,5 @@
+export function HomePage() {
+    return (
+        <h1>Hello Coffee Delivery</h1>
+    )
+}
